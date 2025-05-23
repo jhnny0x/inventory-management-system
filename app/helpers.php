3be402274bae10f84508cc459333e545e6c0f542
jhnny0x <jhnny0x@gmail.com>
2025-05-24 02:34:15 +0800

    init project

---
+++ app/helpers.php
@@ -0,0 +1,16 @@
+<?php
+
+function format_money($money)
+{
+    if (!$money) {
+        return "\$0.00";
+    }
+
+    $money = number_format($money, 2);
+    if (strpos($money, '-')) {
+        $formatted = explode('-', $money);
+        return "-\$$formatted[1]";
+    }
+
+    return "\$$money";
+}
