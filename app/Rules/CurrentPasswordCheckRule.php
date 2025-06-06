cd52b94a13bf2ade6746cebf5a9b7aa407939cd0
jhnny0x <jhnny0x@gmail.com>
2025-06-06 21:25:15 +0800

    remove comment

--- app/Rules/CurrentPasswordCheckRule.php
+++ app/Rules/CurrentPasswordCheckRule.php
@@ -7,23 +7,11 @@ use Illuminate\Contracts\Validation\Rule;
 
 class CurrentPasswordCheckRule implements Rule
 {
-    /**
-     * Determine if the validation rule passes.
-     *
-     * @param  string  $attribute
-     * @param  mixed  $value
-     * @return bool
-     */
     public function passes($attribute, $value)
     {
         return Hash::check($value, auth()->user()->password);
     }
 
-    /**
-     * Get the validation error message.
-     *
-     * @return string
-     */
     public function message()
     {
         return __('The current password field does not match your password');
